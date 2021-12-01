ec0246e6fc70efe99cfac32dde1d384d535b8f8d
Gabriel Gondim <gabriel.gondim@dxtcps000086.local>
2021-12-01 10:51:17 -0300

    Adding logout logic

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,13 +2,24 @@ import React, {useContext} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import FormButton from '../components/FormButton/FormButton';
 import {AuthContext} from '../navigation/AuthProvider';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {RootStack} from '../navigation/routesTypes';
+import {useNavigation} from '@react-navigation/native';
+
+type Props = StackNavigationProp<RootStack, 'Auth'>;
 
 export default function HomeScreen() {
   const {userAuth, logout} = useContext(AuthContext);
+  const navigation = useNavigation<Props>();
+  const {navigate} = navigation;
+  const onLogout = async () => {
+    await logout!();
+    navigate('Auth', {screen: 'Login'});
+  };
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Welcome user {userAuth?.displayName}</Text>
-      <FormButton buttonTitle="Logout" onPress={logout!} />
+      <FormButton buttonTitle="Logout" onPress={onLogout} />
     </View>
   );
 }
