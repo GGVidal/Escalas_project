c6ad5c31cb61dd22a6bc27cdabe691b9b578a803
Gabriel Gondim <gabriel.gondim@dxtcps000086.local>
2021-12-02 15:31:49 -0300

    changes in homescreen

--- src/navigation/HomeStack/HomeStack.tsx
+++ src/navigation/HomeStack/HomeStack.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import HomeScreen from '../../screens/Home/Home';
+import HomeScreen from '../../screens/Home';
 import {RootHomeStack} from './routeTypes';
 const Stack = createStackNavigator<RootHomeStack>();
 
