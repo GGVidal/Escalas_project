225564461770dba64db174c5ebda9dfca80bd678
Gabriel Gondim <gabriel.gondim@dxtcps000086.local>
2021-12-21 16:41:00 -0300

    poc: add table lib in home

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,25 +1,60 @@
-import React, {useContext} from 'react';
-import {Container, Text} from './styles';
+import React, { useContext } from 'react';
+import {
+    Container,
+    RowContainer,
+    RowsContainer,
+    TableContainer,
+    TableWrapperContainer,
+    ColContainer,
+    Text
+} from './styles';
 import FormButton from '../../components/FormButton/FormButton';
-import {AuthContext} from '../../navigation/AuthProvider';
-import {StackNavigationProp} from '@react-navigation/stack';
-import {RootStack} from '../../navigation/routesTypes';
-import {useNavigation} from '@react-navigation/native';
+import { AuthContext } from '../../navigation/AuthProvider';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootStack } from '../../navigation/routesTypes';
+import { useNavigation } from '@react-navigation/native';
 
 type Props = StackNavigationProp<RootStack, 'Auth'>;
 
 export default function HomeScreen() {
-  const {userAuth, logout} = useContext(AuthContext);
-  const navigation = useNavigation<Props>();
-  const {navigate} = navigation;
-  const onLogout = async () => {
-    await logout!();
-    navigate('Auth', {screen: 'Login'});
-  };
-  return (
-    <Container>
-      <Text>Welcome user {userAuth?.displayName}</Text>
-      <FormButton buttonTitle="Logout" onPress={onLogout} />
-    </Container>
-  );
+    const { userAuth, logout } = useContext(AuthContext);
+    const navigation = useNavigation<Props>();
+    const { navigate } = navigation;
+    const tableHead = ['', 'Head1', 'Head2', 'Head3'];
+    const tableTitle = ['Title', 'Title2', 'Title3', 'Title4'];
+    const tableData = [
+        ['1', '2', '3'],
+        ['a', 'b', 'c'],
+        ['1', '2', '3'],
+        ['a', 'b', 'c']
+    ];
+    const onLogout = async () => {
+        await logout!();
+        navigate('Auth', { screen: 'Login' });
+    };
+    return (
+        <Container>
+            <Text>Welcome user {userAuth?.displayName}</Text>
+            <TableContainer borderStyle={{ borderWidth: 1 }}>
+                <RowContainer
+                    data={tableHead}
+                    flexArr={[1, 2, 1, 1]}
+                    textStyle={{ textAlign: 'center' }}
+                />
+                <TableWrapperContainer>
+                    <ColContainer
+                        data={tableTitle}
+                        heightArr={[28, 28]}
+                        textStyle={{ textAlign: 'center' }}
+                    />
+                    <RowsContainer
+                        data={tableData}
+                        flexArr={[2, 1, 1]}
+                        textStyle={{ textAlign: 'center' }}
+                    />
+                </TableWrapperContainer>
+            </TableContainer>
+            <FormButton buttonTitle="Logout" onPress={onLogout} />
+        </Container>
+    );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,14 +1,40 @@
 import { TextProps, ViewProps } from 'react-native';
+import {
+    Table,
+    TableWrapper,
+    Row,
+    Rows,
+    Col
+} from 'react-native-table-component';
 import styled from 'styled-components/native';
 
 export const Container = styled.TouchableOpacity<ViewProps>`
     flex: 1;
-    justify-content: center;
-    align-items: center;
+    padding: 16px;
+    padding-top: 30px;
     background-color: #f5f5f1;
 `;
 
 export const Text = styled.Text<TextProps>`
     font-size: 20px;
     color: #333333;
+    align-self: center;
+`;
+
+export const RowContainer = styled(Row)`
+    height: 40px;
+    background-color: #f1f8ff;
+`;
+
+export const TableContainer = styled(Table)``;
+
+export const TableWrapperContainer = styled(TableWrapper)`
+    flex-direction: row;
+`;
+export const ColContainer = styled(Col)`
+    flex: 1;
+    background-color: #f6f8fa;
+`;
+export const RowsContainer = styled(Rows)`
+    height: 28px;
 `;
